b16cd4042228618f921241c8b04dcf5a57d21011
Nick Genghar <nick.genghar@outlook.com>
2024-09-21 00:00:33 +0800

    Update: Implemented functionality for Windows.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-//demoapp
 use std::io::{self};
 use std::process::Command;
-use std::env::{self};
-use std::os::{self};
+use std::fs::{self};
 
 const HELPER_STRING:[&str; 6] = [
     "[S] Start launcher",
@@ -37,7 +35,14 @@ impl MMcP {
     fn exec(&mut self) {
         match self.minecraft.output() {
             Ok(_) => {
-                println!("Launcher closed. Returning to MMcP.");
+                if cfg!(target_os = "linux") {
+                    println!("Launcher closed. Returning to MMcP.");
+                } else if cfg!(target_os = "windows") {
+                    //The Windows version of the launcher somehow disowned the spawned
+                    //child process MMcP requires. This message here will act as a notice
+                    //until a better implementation made.
+                    println!("MMcP window will stay open while the launcher is opened. Be sure to close the launcher first before doing any other tasks.");
+                }
             },
             Err(e) => {
                 println!("An error occured while starting Minecraft. See error message for help:\n\n {}", e);
@@ -66,13 +71,38 @@ fn modify_mmcp(mmcp: &mut MMcP, buf: &mut String) {
 }
 
 fn main() {
-    //Linux only for now, so let's exit the program immediately if it's
-    //built and ran on other operating system...
-    if !cfg!(target_os = "linux") {
-        return
-    };
+    //Welcome message.
+    println!("MMcP - Make Minecraft Portable");
+
     //setup
-    let minecraft = Command::new("minecraft-launcher");
+    let minecraft:Command;
+    let launcher_dir:String;
+
+    if cfg!(target_os = "windows") {
+        launcher_dir = String::from("C:\\XboxGames\\Minecraft Launcher\\Content\\Minecraft.exe");
+    } else if cfg!(target_os = "linux") {
+        launcher_dir = String::from("minecraft-launcher");
+    } else {
+        println!("Hello there, MMcP does not support your platform. Stay tuned for more info.");
+        return;
+    };
+
+    //Making sure Minecraft Launcher is installed before proceeding.
+    //There will be, issues, if the launcher isn't installed yet...
+    match fs::exists(&launcher_dir) {
+        Ok(r) => {
+            if r {
+                minecraft = Command::new(&launcher_dir);
+            } else {
+                println!("There was an issue picking up the launcher. MMcP does not implemented a function to auto install, therefore it could not proceed any further. Sorry for the inconvenience.");
+                return;
+            }
+        },
+        Err(_) => {
+            println!("Launcher installation not found. MMcP does not implement the necessary features to download and install the launcher yet. Stay tuned for more info.");
+            return;
+        }
+    }
     let mut buf = String::new();
 
     let mut mmcp = MMcP {
@@ -80,8 +110,6 @@ fn main() {
         instance_dir: String::from(""),
         minecraft: minecraft
     };
-
-    println!("MMcP - Make Minecraft Portable");
 
     loop {
         for i in HELPER_STRING {
